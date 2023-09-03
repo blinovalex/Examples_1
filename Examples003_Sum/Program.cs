32b39085544667f986903d6817b60dfc87632e43
Aleksey Blinov <blinov71111@gmail.com>
2023-09-03 11:13:57 +0300

    Изменил

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
-int numberA = 2;
-int numberB = 5;
+﻿using System.Data;
+using System;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+using System;
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
