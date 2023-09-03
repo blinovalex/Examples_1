69c8800aa3bdf0dd866ab53e59e986a98d4c2dd7
Aleksey Blinov <blinov71111@gmail.com>
2023-09-03 19:44:04 +0300

    Examples005_if

---
+++ Examples005_if/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Concurrent;
+using System.Runtime.Serialization;
+using System.Reflection.Emit;
+using System.Data;
+using System;
+Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower()  == "masha")
+{
+
+    Console.WriteLine("Ура, этоже Masha");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    
+
+
+}
